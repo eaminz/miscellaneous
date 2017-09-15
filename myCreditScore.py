53ab5ff144333a0dd6504c86eedafa7bfebda6c9
Eamin Zhang <chizha2@cisco.com>
2017-09-14 20:23:06 -0700

    Scrape KreditKarma (JS rendered) with selenium

--- myCreditScore.py
+++ myCreditScore.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
-# This script fetches my 3 credit scores from KreditKarma and SelfLender
-# TODO: Add proper error handling if it's to be shared
-# TODO: Get Equifax Score.
-# On CreditKarma, scores are rendered by as-yet-unknown JS, not found in page source. May have to use webdriver.
+# This script scrapes my 3 credit scores from KreditKarma and SelfLender
 
-import re
-import requests
-import sys
 from fake_useragent import UserAgent
+import requests
+from selenium import webdriver
+import sys
+from xvfbwrapper import Xvfb
 
 # Get password from command line argument
 if len(sys.argv) < 2 or len(sys.argv[1]) < 8:
@@ -15,30 +13,33 @@ if len(sys.argv) < 2 or len(sys.argv[1]) < 8:
 	sys.exit(1)
 password = sys.argv[1]
 
-# Get TransUnion and Equifax Scores from CreditKarma
-session = requests.Session()
+print "Scraping my credit scores. This could take up to 20 seconds."
 
-resp = session.get("https://www.creditkarma.com/auth/logon")
-stk = resp.text.encode("ascii", "ignore").split("name=\"stk\" value=\"")[1].split("\"")[0]
-credential = {
-	"username": "eamin.zhang@gmail.com",
-	"password": password,
-	"rememberEmail": "1",
-	"stk": stk
-}
-
-session.post("https://www.creditkarma.com/auth/logon", data=credential)
-resp = session.get("https://www.creditkarma.com/tools/credit-score-simulator")
-if "score-value\" >" in resp.text:
-	transunion = resp.text.encode("ascii", "ignore").split("score-value\" >")[1][:3]
-	print "TransUnion:", transunion 
-else:
+# Get TransUnion and Equifax Scores from CreditKarma using selenium webdriver
+display = Xvfb()
+display.start()
+browser = webdriver.Chrome()
+browser.get("https://www.creditkarma.com/auth/logon/")
+browser.find_element_by_id("username").send_keys("eamin.zhang@gmail.com")
+browser.find_element_by_id("password").send_keys(password)
+browser.find_element_by_id("Logon").click()
+browser.implicitly_wait(15)
+try:
+	print "TransUnion:", browser.find_element_by_xpath("//*[@id='__galaxy']/div/main/div[2]/div/div/div/section[1]/div[1]/a[1]") \
+		.find_elements_by_tag_name("text")[6].text
+except:
 	print "Failed to get TransUnion Score from CreditKarma"
+try:
+	print "Equifax:", browser.find_element_by_xpath("//*[@id='__galaxy']/div/main/div[2]/div/div/div/section[1]/div[1]/a[2]") \
+		.find_elements_by_tag_name("text")[6].text
+except:
+	print "Failed to get Equifax Score from CreditKarma"
+browser.quit()
+display.stop()
 
-# Get Experian Score from SelfLender
+# Get Experian Score from SelfLender using requests session
 session = requests.Session()
 headers = {"Referer": "https://www.selflender.com/", "User-Agent": str(UserAgent().chrome)}
-
 resp = session.get("https://www.selflender.com/login", headers=headers)
 token = resp.cookies["_csrf_token"]
 credential = {
@@ -46,11 +47,9 @@ credential = {
 	"password": password,
 	"_csrf_token": token
 }
-
 session.post("https://www.selflender.com/login", data=credential, headers=headers)
 resp = session.get("https://www.selflender.com/home/dashboard", headers=headers)
 if "myCreditScore = " in resp.text:
-	experian = resp.text.encode("ascii", "ignore").split("myCreditScore = ")[1][:3]
-	print "Experian:", experian
+	print "Experian:", resp.text.encode("ascii", "ignore").split("myCreditScore = ")[1][:3]
 else:
 	print "Failed to get Experian Score from SelfLender"
