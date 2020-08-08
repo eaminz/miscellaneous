fc399d3ef8b87476dd92cbe7aad0571000e91c66
Eamin Zhang <eamin.zhang@gmail.com>
2020-08-08 01:59:57 -0700

    Update purgeMetricTable.sh

--- purgeMetricTable.sh
+++ purgeMetricTable.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-let prim_key=900000000
-while [[ $prim_key -ge 0 ]]; do
-    echo "DELETE FROM video_copyright_protection_metrics WHERE prim_key > $prim_key"
-    eval "db -w -u xdb.rm_insights -e 'DELETE FROM video_copyright_protection_metrics WHERE prim_key > $prim_key;'"
-    prim_key=$(($prim_key-1000000))
+#let prim_key=900000000
+#while [[ $prim_key -ge 0 ]]; do
+#    echo "DELETE FROM video_copyright_protection_metrics WHERE prim_key > $prim_key"
+#    eval "db -w -u xdb.rm_insights -e 'DELETE FROM video_copyright_protection_metrics WHERE prim_key > $prim_key;'"
+#    prim_key=$(($prim_key-1000000))
+#done
+
+for i in {1..600}
+do
+    echo "$i"
+    eval "db -w -u xdb.rm_insights -e 'DELETE FROM protection_metrics_time_series LIMIT 1000000;'"
 done
